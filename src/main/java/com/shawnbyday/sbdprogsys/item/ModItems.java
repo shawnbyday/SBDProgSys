dc0f073220d2ca89679d595aa2b0aa4350cf09b1
Shawn Miller <shawn@shawnbyday.com>
2023-08-06 16:16:26 -0500

    Started working on some of the building blocks for leatherworking progression.

--- src/main/java/com/shawnbyday/sbdprogsys/item/ModItems.java
+++ src/main/java/com/shawnbyday/sbdprogsys/item/ModItems.java
@@ -12,6 +12,21 @@ public class ModItems {
 
     public static final RegistryObject<Item> FLINTWORKED = ITEMS.register("flint_worked", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> KNIFEWORKING = ITEMS.register("knife_working", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_AWL = ITEMS.register("lw_awl", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_BEVELER = ITEMS.register("lw_beveler", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_BURNISHER = ITEMS.register("lw_burnisher", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_STRAP_CUTTER = ITEMS.register("lw_strap_cutter", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_ROTARY_CUTTER = ITEMS.register("lw_rotary_cutter", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_GLUE = ITEMS.register("lw_glue", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_MAUL = ITEMS.register("lw_maul", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_PICKING_IRON = ITEMS.register("lw_picking_iron", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_CHISEL = ITEMS.register("lw_chisel", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_PUNCH = ITEMS.register("lw_punch", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_SKIVER = ITEMS.register("lw_skiver", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_STAMP = ITEMS.register("lw_stamp", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_NEEDLE = ITEMS.register("lw_needle", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_THREAD = ITEMS.register("lw_thread", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LW_CUTTING_MAT = ITEMS.register("lw_cutting_mat", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
